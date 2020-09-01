da228f266a4a6c72ef1dbd757546a7c3f7a7b298
XuX26 <ulysse.raillon@hotmail.fr>
2020-09-01 22:47:36 +0200

    Input mouse DestroyWall

---
+++ GFW_2020_Unity/Assets/Script/Controls/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReadInput();
+    }
+
+    void ReadInput()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("mouseDown");
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                BreakWallOnRaycast(hit);
+            }
+        }
+
+    }
+
+    void BreakWallOnRaycast(RaycastHit hit)
+    {
+        Debug.Log(hit.collider.tag);
+        hit.transform.gameObject.SetActive(false);
+    }
+}
