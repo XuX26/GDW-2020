81f481f186ee2382b143ec59bc7f38a0a5d9861b
XuX26 <ulysse.raillon@hotmail.fr>
2020-09-02 23:51:53 +0200

    New Ray + modifs scene

--- GFW_2020_Unity/Assets/Script/Controls/PlayerController.cs
+++ GFW_2020_Unity/Assets/Script/Controls/PlayerController.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool instantDestroyMode;
+    public float maxRangeRayCast;
 
-    // Update is called once per frame
     void Update()
     {
         ReadInput();
@@ -20,19 +16,31 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            Debug.Log("mouseDown");
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.tag == "Wall" )
+            if (!instantDestroyMode)
             {
-                BreakWallOnRaycast(hit);
+                if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.tag == "Wall")
+                {
+                    BreakWallOnRaycast(hit);
+                }
+            }
+
+            else
+            {
+                int layerMask = 1 << 8;
+                RaycastHit[] hits = Physics.RaycastAll(ray, maxRangeRayCast, layerMask);
+                for (int i = 1; i < hits.Length; i++)
+                {
+                    BreakWallOnRaycast(hits[i]);
+                }
+
             }
         }
     }
 
     void BreakWallOnRaycast(RaycastHit hit)
     {
-        Debug.Log(hit.collider.tag);
         hit.transform.gameObject.SetActive(false);
     }
 }
