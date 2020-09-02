5dc7646ea25176df2e7e48e6eb2ad52e3f562acf
XuX26 <ulysse.raillon@hotmail.fr>
2020-09-02 16:38:08 +0200

    Wall creator

---
+++ GFW_2020_Unity/Assets/Script/WallCreator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WallCreator : MonoBehaviour
+{
+    public float gap;
+    public GameObject CubeModel;
+    public Transform firstCubePos; //top left corner
+    public Transform lastCubePos; //bot right corner
+
+    private float cubeSize;
+    private int nbrCubeInLane;
+    private int nbrLane;
+
+
+    void Start()
+    {
+        cubeSize = CubeModel.GetComponent<BoxCollider>().size.x; //only x because it's a square
+        CreateWall();
+    }
+
+    void CreateWall()
+    {
+        SetNbrOfCubes();
+        InstanciateCubes();
+    }
+
+    void SetNbrOfCubes()
+    {
+        float tmpNbrCubesInLane = GetWallLenght() / (gap * cubeSize);
+        nbrCubeInLane = (int)tmpNbrCubesInLane;
+
+        float tmpNbrLane = GetWallWidth() / (gap * cubeSize);
+        nbrLane = (int)tmpNbrLane;
+    }
+
+    void InstanciateCubes()
+    {
+        Debug.Log(nbrCubeInLane);
+        Debug.Log(nbrLane);
+
+        Vector3 NextInstancePose = firstCubePos.localPosition;
+        for (int lane = 1; lane > nbrLane; lane--)
+        {
+            for (int cube = 0; cube < nbrCubeInLane; cube++)
+            {
+                Instantiate(CubeModel, NextInstancePose, Quaternion.identity,this.transform);
+                NextInstancePose.x += gap;
+            }
+
+            NextInstancePose.x = firstCubePos.localPosition.x;
+            NextInstancePose.y -= gap;
+        }
+        Debug.Log("Wallfinished");
+    }
+
+    float GetWallLenght()
+    {
+       Debug.Log(lastCubePos.localPosition.x - firstCubePos.localPosition.x);
+        return (lastCubePos.localPosition.x - firstCubePos.localPosition.x);
+    }
+
+    float GetWallWidth()
+    {
+        return (lastCubePos.localPosition.y - firstCubePos.localPosition.y);
+    }
+
+
+}
