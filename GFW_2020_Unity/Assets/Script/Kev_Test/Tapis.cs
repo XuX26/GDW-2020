3d809d20497cd51c628e8765e4d3a7362d25bed8
Pitter <ikevin77@hotmail.fr>
2020-09-02 11:22:38 +0200

    tapis test

---
+++ GFW_2020_Unity/Assets/Script/Kev_Test/Tapis.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tapis : MonoBehaviour
+{
+    public float speed;
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("coucou");
+        other.gameObject.transform.Translate(Vector3.back * speed); // Vector3.forward
+    }
+
+}
