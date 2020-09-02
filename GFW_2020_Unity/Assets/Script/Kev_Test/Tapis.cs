b895ffc34efc6d46f7c6555a280e30690a817b3b
Pitter <ikevin77@hotmail.fr>
2020-09-02 18:29:02 +0200

    ReReTapis

--- GFW_2020_Unity/Assets/Script/Kev_Test/Tapis.cs
+++ GFW_2020_Unity/Assets/Script/Kev_Test/Tapis.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Tapis : MonoBehaviour
 {
     public float speed;
-   // public Rigidbody rb;
+    public Rigidbody rb;
+    public Vector3 move;
 
     private void OnTriggerStay(Collider other)
     {
         //Debug.Log("coucou");
         if(other.gameObject.CompareTag("Player"))
         {
-            other.gameObject.transform.Translate(gameObject.transform.forward * speed); // Vector3.back
-        }
-        
+            //  other.gameObject.transform.Translate(gameObject.transform.forward * speed); // Vector3.back
+            movement(move);
+        }  
+    }
+
+    void movement(Vector3 direction)
+    {
+        rb.velocity = direction * speed;
     }
 
 }
