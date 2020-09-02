4e51db8ffa11c7fda424a655ae4e872460da0599
XuX26 <ulysse.raillon@hotmail.fr>
2020-09-02 09:33:24 +0200

    Scene

--- GFW_2020_Unity/Assets/Script/Controls/WallCreator.cs
+++ GFW_2020_Unity/Assets/Script/Controls/WallCreator.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class WallCreator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float gap;
+    public GameObject position;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private GameObject firstCube;
+    private float cubeSize;
+
+
 }
