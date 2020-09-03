19633e518f04c68b8eeb74fbdf6d08533c44ec32
Karpy <remi.vedrenne.pro@gmail.com>
2020-09-03 15:46:02 +0200

    UI/Life

---
+++ GFW_2020_Unity/Assets/Script/CanvasManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> Croix;
+    public int life;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            fail();
+        }
+    }
+
+    public void fail()
+    {
+        if(life==3)
+        {
+            //Ecran de Score
+            Debug.Log("Score");
+        }
+        else
+        {
+            life += 1;
+            Croix[life - 1].SetActive(true);
+        }
+        
+    }
+}
