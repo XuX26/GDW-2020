419445151f70b5464c4ab606240b844747404495
Pitter <ikevin77@hotmail.fr>
2020-09-03 18:13:34 +0200

    Score

---
+++ GFW_2020_Unity/Assets/Script/Kev_Test/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Wall"))
+        {
+            gameObject.GetComponent<Rigidbody>();
+        }
+    }
+}
