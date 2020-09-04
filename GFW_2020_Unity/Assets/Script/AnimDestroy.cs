68374af30c80517443f123c49a1bd204dd7d7b20
Karpy <remi.vedrenne.pro@gmail.com>
2020-09-04 09:53:00 +0200

    Debug Anim

---
+++ GFW_2020_Unity/Assets/Script/AnimDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float TimeAnim;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeAnim -= Time.deltaTime;
+
+        if(TimeAnim<=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
